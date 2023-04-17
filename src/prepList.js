88e31fbba4e3a176ae41594c6bc4e634ecf659b1
Michael A. Yount <cheshirekatsmile101@gmail.com>
2023-04-17 06:51:25 -0400

    added prep state

---
+++ src/prepList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+
+function Preplist(newPreps) {
+    // the whole solution is the "prep".
+    const [prep, setPrep] = React.useState(prep)
+}     let prep = prep.map()
+
+export default Preplist
