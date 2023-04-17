280d67e98f13b2f22d15c62ec5cec3499dff6e5c
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-04-17 08:49:22 -0400

    added icons started App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./styles.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+          <h1>Kitchen Prep</h1>
+        </div>
+      
+      </div>
     </div>
+    
   );
 }
+
 
 export default App;
